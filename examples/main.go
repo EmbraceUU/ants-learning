9dcb5b50131e18826d162b0cf94094d2e0739ee6
EmbraceUU <1014435409@qq.com>
2020-07-14 17:04:56 +0800

    了解ants的使用方法

---
+++ examples/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/panjf2000/ants/v2"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+/*
+官方案例
+*/
+
+var sum int32
+
+func myFunc(i interface{}) {
+	n := i.(int32)
+	atomic.AddInt32(&sum, n)
+	fmt.Printf("run with %d\n", n)
+}
+
+func demoFunc() {
+	time.Sleep(10 * time.Millisecond)
+	fmt.Println("Hello World!")
+}
+
+func main() {
+	// 延迟加载, 回收ants
+	defer ants.Release()
+
+	runTimes := 1000
+
+	var wg sync.WaitGroup
+	// 使用普通协程池
+	syncCalculateSum := func() {
+		demoFunc()
+		wg.Done()
+	}
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		_ = ants.Submit(syncCalculateSum)
+	}
+	wg.Wait()
+	fmt.Printf("running goroutines: %d\n", ants.Running())
+	fmt.Printf("finish all tasks.\n")
+
+	// 使用自定义函数的协程池
+	// 初始容量为10, 过期时间为1s
+	p, _ := ants.NewPoolWithFunc(10, func(i interface{}) {
+		myFunc(i)
+		wg.Done()
+	})
+	defer p.Release()
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		_ = p.Invoke(int32(i))
+	}
+	wg.Wait()
+	fmt.Printf("running goroutines: %d\n", p.Running())
+	fmt.Printf("finish all tasks, result is %d\n", sum)
+	if sum != 499500 {
+		panic("the final result is wrong!!!")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module ants-learning
+
+go 1.14
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/stretchr/testify v1.4.0
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+	github.com/panjf2000/ants/v2 v2.4.1
+)
